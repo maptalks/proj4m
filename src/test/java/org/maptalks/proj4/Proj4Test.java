c230fab1b9a7c626072d12dbcc0b2d86f4957f3e
fuzhen <fuzhen@maptalks.org>
2016-02-04 17:22:53 +0800

    update unit test

--- src/test/java/org/maptalks/proj4/Proj4Test.java
+++ src/test/java/org/maptalks/proj4/Proj4Test.java
@@ -2,7 +2,10 @@ package org.maptalks.proj4;
 
 import org.testng.annotations.Test;
 
+import java.util.Arrays;
+
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
 
 public class Proj4Test {
 
@@ -61,6 +64,34 @@ public class Proj4Test {
         double tolerance = 1e-7;
         assertEquals(coord[0], 120.0, tolerance);
         assertEquals(coord[1], 30.0, tolerance);
+    }
+
+    @Test
+    public void testGcj02ToBaidu() throws Exception {
+        double[][] expected = new double[][]{
+            {114.69490414027017,33.639096507711685},
+            {114.69488614273101,33.63804850387785},
+            {114.69500713986416,33.63794251496537},
+            {114.69578412001135,33.63793958798685},
+            {114.6959281162725,33.637965601694006},
+            {114.69751307493384,33.637957753486745}
+        };
+
+
+        Proj4 proj = new Proj4("+proj=longlat +datum=GCJ02", "+proj=longlat +datum=BD09");
+        double[][] lonlats = new double[][]{
+            {114.68837663801743, 33.63312016454496},
+            {114.68835840204522, 33.632072446353945},
+            {114.68848002806972, 33.63196427051657},
+            {114.68926112541861, 33.63194729708501},
+            {114.68940588838505, 33.6319707051534},
+            {114.69099925796665, 33.63193416046613}
+        };
+
+        for (int i = 0; i < lonlats.length; i++) {
+            double[] coord = proj.forward(lonlats[i]);
+            assertTrue(Arrays.equals(expected[i], coord));
+        }
     }
 
 }
