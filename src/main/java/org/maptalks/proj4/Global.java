d2335af72a6330b734e2a55f092b392197c1ea6b
wsw <wsw0108@gmail.com>
2016-02-15 22:32:50 +0800

    typo....

--- src/main/java/org/maptalks/proj4/Global.java
+++ src/main/java/org/maptalks/proj4/Global.java
@@ -10,8 +10,8 @@ public class Global {
         def("EPSG:4326", "+proj=longlat +datum=WGS84 +no_defs");
         def("WGS84", "+proj=longlat +datum=WGS84 +no_defs");
         def("EPSG:3857", "+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext +no_defs");
-        def("GCJ02", "+proj=longlat +dataum=GCJ02");
-        def("GCJ02MC", "+proj=merc +dataum=GCJ02");
+        def("GCJ02", "+proj=longlat +datum=GCJ02");
+        def("GCJ02MC", "+proj=merc +datum=GCJ02");
         def("BD09LL", "+proj=longlat +datum=BD09");
         def("BD09MC", "+proj=bmerc +datum=BD09");
         def("IDENTITY", "+proj=identity +no_defs");
